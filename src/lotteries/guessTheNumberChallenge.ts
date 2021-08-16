c14e9d830b4cd27e27fe540e033db8b26092d8f5
foxytanuki <biz@foxytanuki.dev>
2021-08-16 19:10:06 +0900

    Complete 'Guess the number'

---
+++ src/lotteries/guessTheNumberChallenge.ts
@@ -0,0 +1,25 @@
+import { utils, PayableOverrides } from "ethers";
+import { GuessTheNumberChallenge__factory } from "../../types/ethers-contracts";
+import { handleError, getWallet } from "../utils";
+
+const CONTRACT_ADDRESS = "0xB841e47509C3Ceba3E9d6724EEAED405CC16106f";
+
+const main = async () => {
+  const wallet = getWallet();
+
+  const guesser = GuessTheNumberChallenge__factory.connect(
+    CONTRACT_ADDRESS,
+    wallet
+  );
+  const ans = 42;
+  const overrides: PayableOverrides = {
+    value: utils.parseEther("1"),
+    gasLimit: "50000",
+  };
+
+  const result = await guesser.guess(ans, overrides);
+
+  console.log(result);
+};
+
+main().catch(handleError);
