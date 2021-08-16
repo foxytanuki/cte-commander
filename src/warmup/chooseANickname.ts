83a988b3f762a1a0d1d182722fdae96ee3f1bbed
foxytanuki <biz@foxytanuki.dev>
2021-08-16 17:46:13 +0900

    Complete 'Choose a nickname'

---
+++ src/warmup/chooseANickname.ts
@@ -0,0 +1,21 @@
+import { utils } from "ethers";
+import { CaptureTheEther__factory } from "../../types/ethers-contracts";
+import { handleError, getWallet } from "../utils";
+
+const CONTRACT_ADDRESS = "0x71c46Ed333C35e4E6c62D32dc7C8F00D125b4fee";
+
+const main = async () => {
+  const wallet = getWallet();
+
+  const nickname = "foxyTanuki";
+  const nicknameBytes32 = utils.formatBytes32String(nickname);
+
+  const cte = CaptureTheEther__factory.connect(CONTRACT_ADDRESS, wallet).catch(
+    handleError
+  );
+  const result = await cte.setNickname(nicknameBytes32).catch(handleError);
+
+  console.log(result);
+};
+
+main().catch(handleError);
