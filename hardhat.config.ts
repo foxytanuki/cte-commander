2aedbf132afe706c2c55d6604f9b125e15e88227
foxytanuki <biz@foxytanuki.dev>
2022-02-20 21:58:03 +0900

    Cleaning

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,7 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
     ropsten: {
-      url: process.env.RPC_HOST,
+      url: 'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
     },
   },
 };
